58ad1ab9627d038d6590d15e2323d7d3d87d9df5
kareem-shaban <kareemmedo432@gmail.com>
2024-08-10 15:27:40 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// function formVaLidate() {
+//     var Name = document.getElementById('form2Example1').value;
+//     var Password = document.getElementById('form2Example2').value;
+//     var Error = document.getElementById('error');
+//     var Text = '';
+
+    
+  
+//        if (Name.length < 15){
+//         Text = "please enter 15 number in email";
+//         Error.innerHTML = Text;
+//         return false;
+//     }
+
+//     if (Password.length < 10){
+//         Text = "please enter 10 number in password";
+//         Error.innerHTML = Text;
+//         return false;
+//     }
+//     else{
+//         alert('Form Submitted Successfully');
+//         return true;
+//     }
+
+// };
